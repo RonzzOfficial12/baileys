1faec60a554bc403e2f41f4643aed1da5e1894b1
RonzzOfficial12 <renianisazas@gmail.com>
2026-03-10 09:39:38 +0700

    Delete logger.js

--- logger.js
+++
@@ -1,7 +0,0 @@
-const chalk = require("chalk")
-
-module.exports = {
-info: (msg) => console.log(chalk.blue("[INFO]"), msg),
-success: (msg) => console.log(chalk.green("[SUCCESS]"), msg),
-error: (msg) => console.log(chalk.red("[ERROR]"), msg)
-}
